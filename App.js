97e8e8a087532b2495d7e05dac5ced60b1854078
Neeraj tiwari <nt98924@gmail.com>
2023-03-18 18:34:58 +0530

    Game completed

--- App.js
+++ App.js
@@ -25,5 +25,60 @@ function addGo(e){
     e.target.append(goDisplay)
     go = go === "circle" ? "cross" : "circle"
     info.textContent = "it is now " + go + " 's go.";
-    e.target.removeEventListener("click", addGo)
+    e.target.removeEventListener("click", addGo);
+    checkScore()
 }
+
+function checkScore(){
+  const allSquere =  document.querySelectorAll('.squere')
+//   console.log(allSquere)
+    const winningcombos = [
+        [0,1,2],[3,4,5],[6,7,8],
+        [0,3,6],[1,4,7],[2,5,8],
+        [0,4,8],[2,4,6]
+    ]
+    console.log(allSquere[4])
+
+    winningcombos.forEach(Array => {
+     const circleWins =    Array.every(cell =>
+             allSquere[cell].firstChild?.classList.contains('circle'))
+
+             if (circleWins) {
+                info.textContent = "Circle wins!"
+                allSquere.forEach(squere => squere.replaceWith(squere.cloneNode(true)))
+                return      
+             }
+            //  -----------------------
+             winningcombos.forEach(Array => {
+                const crossWins =    Array.every(cell =>
+                        allSquere[cell].firstChild?.classList.contains('cross'))
+           
+                        if (crossWins) {
+                           info.textContent = "Cross wins!"
+                           allSquere.forEach(squere => squere.replaceWith(squere.cloneNode(true)))
+                           return      
+                        }
+               })
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
